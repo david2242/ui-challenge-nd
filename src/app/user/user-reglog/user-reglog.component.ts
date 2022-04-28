df0eacf7ee66dc5d64fd7e4b40c7ff99b19dfd08
david2242 <88514735+david2242@users.noreply.github.com>
2022-04-28 23:08:51 +0200

    double-password works

--- src/app/user/user-reglog/user-reglog.component.ts
+++ src/app/user/user-reglog/user-reglog.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 import { CreateUser, UserEmailPasswordInterface, UserInterface } from 'src/app/model/user';
 import { AuthService } from 'src/app/service/auth.service';
 import { UserService } from 'src/app/service/user.service';
@@ -19,29 +19,38 @@ export class UserReglogComponent implements OnInit {
   public userForm: FormGroup = new FormGroup({
     userEmail: new FormControl('', [Validators.required, Validators.email]),
     userPassword: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    userPasswordAgain: new FormControl('', [Validators.required]),
     userName: new FormControl('', [Validators.required, Validators.minLength(5)]),
-  });
-  get userEmail () {
+  },
+  {
+    // validators: this.confirmPasswordMatch('userPassword', 'userPasswordAgain')
+  }
+  )
+
+  get userEmail() {
     return this.userForm.get('userEmail');
   }
-  get userPassword () {
+  get userPassword() {
     return this.userForm.get('userPassword');
   }
-  get userName () {
+  get userPasswordAgain() {
+    return this.userForm.get('userPasswordAgain');
+  }
+  get userName() {
     return this.userForm.get('userName');
   }
 
-  
+
   // LOGIN
   public loginForm: FormGroup = new FormGroup({
     loginEmail: new FormControl('', [Validators.required, Validators.email]),
     loginPassword: new FormControl('', [Validators.required, Validators.minLength(5)]),
   });
-  
-  get loginEmail () {
+
+  get loginEmail() {
     return this.loginForm.get('loginEmail');
   }
-  get loginPassword () {
+  get loginPassword() {
     return this.loginForm.get('loginPassword');
   }
 
@@ -79,7 +88,7 @@ export class UserReglogComponent implements OnInit {
     this.auth.login(loginData).toPromise().then(
       userResponse => {
         this.showSuccessLogin(this.auth.currentUserValue.username),
-        this.router.navigate(['']);
+          this.router.navigate(['']);
       },
       err => {
         this.showError(this.createErrorMessage(err.error.errors));
@@ -127,4 +136,35 @@ export class UserReglogComponent implements OnInit {
     return (formControl!.invalid && formControl!.dirty);
   }
 
+  //PASSWORD CONFIRM 
+  passwordHelper(): string {
+    if (this.userPasswordAgain!.pristine) return 'form-control';
+    if (((this.userPassword!.value == this.userPasswordAgain!.value) && this.userPasswordAgain!.value)) return 'form-control is-valid';
+    if (((this.userPassword!.value != this.userPasswordAgain!.value) && this.userPasswordAgain!.dirty)) return 'form-control is-invalid';
+    return 'form-control';
+  }
+  passwordSmallTextHelper(): boolean | undefined{
+    return ((this.userForm.get('userPassword')?.value != this.userForm.get('userPasswordAgain')?.value) && this.userForm.get('userPasswordAgain')?.dirty);
+  }
+
+  
 }
+
+
+// interface ValidatorFn {
+//   (control: AbstractControl): ValidationErrors | null
+//   confirmPasswordMatch(controlName: string, matchingControlName: string): any {
+//     return (formGroup: FormGroup) => {
+//       const control = formGroup.controls[controlName];
+//       const matchingControl = formGroup.controls[matchingControlName];
+
+//       if (control.value != matchingControl.value) {
+//         matchingControl.setErrors({comfirmPasswordMatch: true});
+//       } else {
+//         matchingControl.setErrors(null);
+//       }
+//     }
+//   }
+// }
+
+
